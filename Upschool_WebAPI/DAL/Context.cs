524b60a2112f17db502c3bcf6a329ec74a21ab8c
mervegebsy <mervegebespy@gmail.com>
2023-01-20 06:27:33 +0300

    Add project files.

---
+++ Upschool_WebAPI/DAL/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Upschool_WebAPI.DAL.Entities;
+
+namespace Upschool_WebAPI.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-GTA587B\\SQLEXPRESS;initial catalog=UpSchoolApi;integrated security=true;");
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
